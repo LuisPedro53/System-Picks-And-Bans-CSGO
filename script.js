a316f7f529519eb880f17c15c5988ee17a1e9384
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2023-03-26 10:50:20 -0300

    mudança da overpass

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const team1 = "FALLEN";
 const team2 = "TRYBE";
 let turn = team1;
 
-let mappool = ["Train", "Mirage", "Cache", "Inferno", "Dust2"];
+let mappool = ["Train", "Mirage", "Overpass", "Inferno", "Dust2"];
 
 
 const turnText = document.querySelector("#turnText");
